33ada5230b007e4a46777d6b0ac1097418872346
Matthias Kestenholz <mk@feinheit.ch>
2016-11-22 14:37:06 +0100

    Add a base user and user manager model for email authentication

--- authlib/base.py
+++ authlib/base.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import models as auth_models
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+
 class OAuthClient(object):
     def __init__(self, request):
         self._request = request
@@ -7,3 +13,54 @@ class OAuthClient(object):
 
     def get_user_data(self):
         pass
+
+
+class BaseUserManager(auth_models.BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Email missing')
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class BaseUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    email = models.EmailField(
+        _('email'),
+        max_length=254,
+        unique=True,
+    )
+    is_active = models.BooleanField(_('is active'), default=True)
+    is_staff = models.BooleanField(_('is staff'), default=False)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = BaseUserManager()
+
+    class Meta:
+        abstract = True
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
