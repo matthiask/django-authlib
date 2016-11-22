6285ecf42ec1af8ce8abd8f68b9ca54a29c332d1
Matthias Kestenholz <mk@feinheit.ch>
2016-11-22 14:57:52 +0100

    Add views for handling the OAuth2 clients and for logging out

--- authlib/views.py
+++ authlib/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.http import is_safe_url
+from django.utils.translation import ugettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.debug import sensitive_post_parameters
 
@@ -12,7 +13,7 @@ def retrieve_next(request):
     return next if is_safe_url(url=next, host=request.get_host()) else None
 
 
-def post_login_response(request):
+def post_login_response(request, new_user):
     return redirect(retrieve_next(request) or settings.LOGIN_REDIRECT_URL)
 
 
@@ -37,3 +38,49 @@ def login(request, template_name='registration/login.html',
     if request.GET.get('next'):
         response.set_cookie('next', request.GET['next'], max_age=600)
     return response
+
+
+@never_cache
+def oauth2(request, client_class, post_login_response=post_login_response):
+    User = auth.get_user_model()
+    client = client_class(request)
+
+    if all(key not in request.GET for key in ('code', 'oauth_token')):
+        return redirect(client.get_authentication_url())
+
+    user_data = client.get_user_data()
+
+    if user_data.get('email'):
+        email = user_data.pop('email')
+        new_user = False
+
+        if not User.objects.filter(email=email).exists():
+            User.objects.create(
+                email=email,
+                **user_data
+            )
+            messages.success(
+                request,
+                _('Welcome! Please fill in your details.'))
+            new_user = True
+
+        user = auth.authenticate(email=email)
+        if user and user.is_active:
+            auth.login(request, user)
+        else:
+            messages.error(
+                request,
+                _('No user with email address %s found.') % email)
+
+        return post_login_response(request, new_user)
+
+    return redirect('login')
+
+
+@never_cache
+def logout(request):
+    auth.logout(request)
+    messages.success(
+        request,
+        _('You have been signed out.'))
+    return redirect('login')
