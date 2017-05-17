af552e527299d1c84a588ae30ccdfe2acc60baf6
Matthias Kestenholz <mk@feinheit.ch>
2017-05-17 13:00:15 +0200

    Omit migrations when calculating coverage

--- tests/cov.sh
+++ tests/cov.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-venv/bin/coverage run --branch --include="*authlib/*" --omit="*tests*" ./manage.py test -v 2 testapp
+venv/bin/coverage run --branch --include="*authlib/*" --omit="*tests*,*migrations*" ./manage.py test -v 2 testapp
 venv/bin/coverage html
