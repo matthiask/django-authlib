15ad055dfe92c85cb9f8fd5d27619159aaa517ba
Matthias Kestenholz <mk@feinheit.ch>
2017-05-17 11:10:46 +0200

    Do not specify the exact auth migration name

--- authlib/little_auth/migrations/0001_initial.py
+++ authlib/little_auth/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0008_alter_user_username_max_length'),
+        ('auth', '__latest__'),
     ]
 
     operations = [
