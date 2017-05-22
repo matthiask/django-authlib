3d024ef771f338b5d1725650f24e4b842b11db5f
Matthias Kestenholz <mk@feinheit.ch>
2017-05-22 11:36:46 +0200

    Add a positional request argument to EmailBackend.authenticate

--- authlib/backends.py
+++ authlib/backends.py
@@ -1,4 +1,13 @@
+from django import VERSION
 from django.contrib.auth import get_user_model
+
+
+def _by_email(email):
+    User = get_user_model()
+    try:
+        return User.objects.get(email=email, is_active=True)
+    except User.DoesNotExist:
+        pass
 
 
 class EmailBackend(object):
@@ -9,12 +18,9 @@ class EmailBackend(object):
         except User.DoesNotExist:
             return None
 
-    def authenticate(self, email):
-        User = get_user_model()
-        try:
-            return User.objects.get(
-                email=email,
-                is_active=True,
-            )
-        except User.DoesNotExist:
-            pass
+    if VERSION < (1, 11):
+        def authenticate(self, email):
+            return _by_email(email)
+    else:
+        def authenticate(self, request, email):
+            return _by_email(email)
