221316ee029b1e747144b3b7dfdea2d4693348a8
Matthias Kestenholz <mk@feinheit.ch>
2018-11-17 18:58:27 +0100

    Remove a backwards compatibility shim for Django<1.11

--- authlib/views.py
+++ authlib/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from django.utils.http import is_safe_url
-from django.utils.inspect import func_supports_parameter
 from django.utils.translation import ugettext as _, ugettext_lazy
 from django.views.decorators.cache import never_cache
 from django.views.decorators.debug import sensitive_post_parameters
@@ -32,11 +31,7 @@ def set_next_cookie(view):
 
 def retrieve_next(request):
     next = request.COOKIES.get(REDIRECT_COOKIE_NAME)
-    if func_supports_parameter(is_safe_url, "allowed_hosts"):  # Django 1.11
-        kw = {"allowed_hosts": {request.get_host()}}
-    else:
-        kw = {"host": request.get_host()}
-    return next if is_safe_url(url=next, **kw) else None
+    return next if is_safe_url(url=next, allowed_hosts={request.get_host()}) else None
 
 
 @positional(1)
