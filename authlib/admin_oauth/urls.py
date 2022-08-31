972ba0bcd3c8d1b7b30eb37a346d089a6d58327e
Matthias Kestenholz <mk@feinheit.ch>
2022-08-31 15:31:43 +0200

    Hooks

--- authlib/admin_oauth/urls.py
+++ authlib/admin_oauth/urls.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 
 from authlib.admin_oauth.views import admin_oauth
 
 
-urlpatterns = [re_path(r"^admin/__oauth__/$", admin_oauth, name="admin_oauth")]
+urlpatterns = [path("admin/__oauth__/", admin_oauth, name="admin_oauth")]
--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.shortcuts import render
-from django.urls import include, re_path
+from django.urls import include, path, re_path
 from testapp.views import custom_verification, custom_verification_code
 
 from authlib import views
@@ -12,36 +12,36 @@ from authlib.twitter import TwitterOAuthClient
 urlpatterns = [
     re_path(r"", include("authlib.admin_oauth.urls")),
     re_path(r"^admin/", admin.site.urls),
-    re_path(r"^404/$", lambda request: render(request, "404.html")),
-    re_path(r"^login/$", views.login, name="login"),
-    re_path(
-        r"^oauth/facebook/$",
+    path("404/", lambda request: render(request, "404.html")),
+    path("login/", views.login, name="login"),
+    path(
+        "oauth/facebook/",
         views.oauth2,
         {"client_class": FacebookOAuth2Client},
         name="accounts_oauth_facebook",
     ),
-    re_path(
-        r"^oauth/google/$",
+    path(
+        "oauth/google/",
         views.oauth2,
         {"client_class": GoogleOAuth2Client},
         name="accounts_oauth_google",
     ),
-    re_path(
-        r"^oauth/twitter/$",
+    path(
+        "oauth/twitter/",
         views.oauth2,
         {"client_class": TwitterOAuthClient},
         name="accounts_oauth_twitter",
     ),
-    re_path(r"^email/$", views.email_registration, name="email_registration"),
-    re_path(
-        r"^email/(?P<code>[^/]+)/$",
+    path("email/", views.email_registration, name="email_registration"),
+    path(
+        "email/<str:code>/",
         views.email_registration,
         name="email_registration_confirm",
     ),
-    re_path(r"^logout/$", views.logout, name="logout"),
-    re_path(r"^custom/$", custom_verification),
-    re_path(
-        r"^custom/(?P<code>[^/]+)/$",
+    path("logout/", views.logout, name="logout"),
+    path("custom/", custom_verification),
+    path(
+        "custom/<str:code>/",
         custom_verification_code,
         name="custom_verification_code",
     ),
